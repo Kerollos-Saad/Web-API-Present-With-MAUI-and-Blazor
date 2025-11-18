97dff43495127be438521aef0e78d783f0e39004
Kerollos-Saad <kirloussaad7788@gmail.com>
2025-11-19 00:04:37 +0200

    .Net MAUI Consumer

---
+++ Presentation/Demo.MauiApiConsumer/MauiProgram.cs
@@ -0,0 +1,31 @@
+﻿using Demo.ApiClient;
+using Demo.ApiClient.IoC;
+using Microsoft.Extensions.Logging;
+
+namespace Demo.MauiApiConsumer
+{
+	public static class MauiProgram
+	{
+		public static MauiApp CreateMauiApp()
+		{
+			var builder = MauiApp.CreateBuilder();
+			builder
+				.UseMauiApp<App>()
+				.ConfigureFonts(fonts =>
+				{
+					fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+					fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+				});
+
+			//builder.Services.AddDemoApiClientService(x => x.ApiBaseAddress = "http://10.0.2.2:5015/"); // Android emulator
+			builder.Services.AddDemoApiClientService(x => x.ApiBaseAddress = "https://localhost:44300/");
+			builder.Services.AddTransient<MainPage>();
+
+#if DEBUG
+			builder.Logging.AddDebug();
+#endif
+
+			return builder.Build();
+		}
+	}
+}
---
+++ Presentation/Demo.MauiApiConsumer/Pages/MainPage.xaml.cs
@@ -0,0 +1,63 @@
+﻿using Demo.ApiClient;
+using Demo.ApiClient.Models.ApiModels;
+using Demo.MauiApiConsumer.Pages;
+
+namespace Demo.MauiApiConsumer
+{
+	public partial class MainPage : ContentPage
+	{
+		private readonly DemoApiClientService _apiClient;
+
+		public MainPage(DemoApiClientService apiClientService)
+		{
+			InitializeComponent();
+			_apiClient = apiClientService;
+		}
+
+		private async Task LoadProducts()
+		{
+			var products = await _apiClient.GetProductsAsync();
+			productListView.ItemsSource = products;
+		}
+
+		protected override async void OnAppearing()
+		{
+			base.OnAppearing();
+			await LoadProducts();
+		}
+
+		private async void btnAdd_Clicked(object? sender, EventArgs e)
+		{
+			await Navigation.PushModalAsync(new AddEditProduct(_apiClient, null));
+		}
+
+		private async void btnShowProducts_Clicked(object? sender, EventArgs e)
+		{
+			await LoadProducts();
+		}
+
+		private async void productListView_SelectionChanged(object? sender, SelectionChangedEventArgs e)
+		{
+			var product = e.CurrentSelection.FirstOrDefault() as Product;
+			if (product == null)
+				return;
+
+			// Clear selection immediately to allow re-selecting the same item
+			((CollectionView)sender).SelectedItem = null;
+
+			// Show action sheet
+			var action = await DisplayActionSheet("Action", "Cancel", null, "Edit", "Delete");
+
+			switch (action)
+			{
+				case "Edit":
+					await Navigation.PushModalAsync(new AddEditProduct(_apiClient, product));
+					break;
+				case "Delete":
+					await _apiClient.DeleteProduct(product.Id);
+					await LoadProducts();
+					break;
+			}
+		}
+	}
+}
