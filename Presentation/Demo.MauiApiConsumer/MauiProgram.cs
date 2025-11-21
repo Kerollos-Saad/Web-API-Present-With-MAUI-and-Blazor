ab35107e314aaeff7ed0c86adcc574c68e3f44dd
Kerollos-Saad <kirloussaad7788@gmail.com>
2025-11-21 17:58:02 +0200

    Auto-select API URL MAUI App

--- Presentation/Demo.MauiApiConsumer/MauiProgram.cs
+++ Presentation/Demo.MauiApiConsumer/MauiProgram.cs
@@ -17,8 +17,28 @@ namespace Demo.MauiApiConsumer
 					fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
 				});
 
-			//builder.Services.AddDemoApiClientService(x => x.ApiBaseAddress = "http://10.0.2.2:5015/"); // Android emulator
-			builder.Services.AddDemoApiClientService(x => x.ApiBaseAddress = "https://localhost:44300/");
+			//Windows	https://localhost:44300/
+			//Android emulator    http://10.0.2.2:5015/
+			//Real Android device http://YOUR_PC_IP:5015/
+			//iOS Simulator   http://localhost:5015/
+			//Real iPhone device http://YOUR_PC_IP:5015/
+
+			string apiUrl = "";
+
+#if ANDROID
+			// Android emulator loopback to host
+			apiUrl = "http://10.0.2.2:5015/";
+#elif WINDOWS
+            apiUrl = "https://localhost:44300/";
+#elif IOS
+            // For iOS simulator, localhost loopback is different
+            apiUrl = "http://localhost:5015/";
+#else
+            // default (real device must use real network IP)
+            apiUrl = "http://YOUR_PC_LOCAL_IP:5015/";
+#endif
+
+			builder.Services.AddDemoApiClientService(x => x.ApiBaseAddress = apiUrl);
 			builder.Services.AddTransient<MainPage>();
 
 #if DEBUG
